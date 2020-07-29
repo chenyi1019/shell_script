a31724fd33985d9213ec2a4fe61916e04a18477e
LJChen1018 <LJChen1018@gmail.com>
2020-07-29 16:10:40 +0800

    second

--- auto_mysql.sh
+++ auto_mysql.sh
@@ -3,3 +3,9 @@
 echo -e "Start scripts ... \n"
 apt install mysql-client-core-8.0
 echo "The mysql install success"
+echo "The mysql install success"
+echo "The mysql install success"
+echo "The mysql install success"
+echo "The mysql install success"
+echo "The mysql install success"
+echo "The mysql install success
